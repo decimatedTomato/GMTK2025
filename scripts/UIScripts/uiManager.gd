0ae6384044de7504901185b2f7ca6ca9c67e8e0b
mugattarova <mugattarova@gmail.com>
2025-08-03 14:46:21 +0300

    Added menu addon, some pause menu implementation, zoom is now done in player camera, not globally (for menus)

---
+++ scripts/UIScripts/uiManager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@onready var pauseMenu
+@onready var pauser
+@onready var pauseButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pauseMenu = $"../PauseLayer/PauseMenu"
+	pauser = $"../Pauser"
+	pauseButton = $"MarginContainer/HBoxContainer/PauseButton"
+	pauseButton.focus_mode = Button.FOCUS_NONE
+	pauseButton.process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+		pass
+
+
+func _on_pause_button_button_down() -> void:
+	if pauseMenu.hidden:
+		pauser._pause()
+		pauseMenu.show()
+	else:
+		pauser._unpause()
+		pauseMenu.hide()
