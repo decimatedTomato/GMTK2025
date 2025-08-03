6f2b9017724e1570b32643273c7491673fc26bef
nik <niklas.harnish@hotmail.com>
2025-08-03 15:53:07 +0200

    fixed some stuf

--- scripts/check_point.gd
+++ scripts/check_point.gd
@@ -7,12 +7,13 @@ var spawnPoints: Array
 @export var checkpoint_id: int;
 
 func _ready() -> void:
-	spawnPoints = find_children("*", "Node2D") as Array[Node2D]
+	spawnPoints = find_children("*", "Node2D", false, true) as Array[Node2D]
 
 func _on_body_entered(_body: Node2D) -> void:
 	checkpoint_reached.emit(checkpoint_id);
 
 func _get_next_spawn_point():
+	print("checkpoint id:", checkpoint_id)
 	var index = randi_range(0, spawnPoints.size() - 1)
 	var spawnPosition = spawnPoints[index].global_position
 	spawnPoints.remove_at(index)
--- scripts/spawn_manager.gd
+++ scripts/spawn_manager.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @export var INITIAL_CHECKPOINT: int
-@export var checkPoints: Array[CheckPoint];
-
+var checkPoints: Array;
 var currentCheckpoint:int;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	checkPoints = find_children("*", "CheckPoint", false) as Array[CheckPoint];
+	# on_checkpoint_changed(INITIAL_CHECKPOINT);
 	currentCheckpoint = INITIAL_CHECKPOINT;
 
 func on_checkpoint_changed(checkpoint: int):
